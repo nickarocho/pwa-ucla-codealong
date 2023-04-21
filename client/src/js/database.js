b7ca49f815aee518a81871bfe8a530b1b38c9e95
nickarocho <nick@boldlyfine.com>
2023-04-20 21:34:14 -0700

    dirty commit, not finished, pre heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,44 @@
 // TODO: Install the following package:
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
+const initdb = async () =>
+  openDB("contactDB", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("contact")) {
+        console.log("contact object store already exists");
+        return;
+      }
 
-};
-
+      db.createObjectStore("contact", { keyPath: "id", autoIncrement: true });
+      console.log("contact DB created!!!!!!!!!!!");
+    },
+  });
 
 // TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
+export const postDb = async (name, home, cell, email) => {
+  try {
+    const contactDb = await openDB("contactDB", 1);
+    const tx = contactDb.transaction("contact", "readwrite");
+    const store = tx.objectStore("contact");
+    const request = store.add({
+      name,
+      home_phone: home,
+      cell_phone: cell,
+      email,
+    });
+
+    const result = await request;
+    console.log("data saved!", result);
+  } catch (error) {
+    console.error("postDb error:", error);
+  }
 };
 
 // TODO: Complete the getDb() function below:
-export const getDb = async () => {
-  
-};
+export const getDb = async () => {};
 
 // TODO: Complete the deleteDb() function below:
-export const deleteDb = async (id) => {
-  
-};
+export const deleteDb = async (id) => {};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,21 +1,76 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js',
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      cards: "./src/js/cards.js",
     },
 
     // TODO: Add the correct output config for the bundled files
-    output: {},
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
 
     // TODO: Add HTMLWebackPlugin, InjectManifest, and WebpackPwaManifest plugins
-    plugins: [],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Contact Cards",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Contact Cards",
+        short_name: "Contact",
+        description: "Never forget your contacts!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
 
     // TODO: Add rules here for the CSS and Babel loaders to compile our files properly
-    module: {},
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
   };
 };
